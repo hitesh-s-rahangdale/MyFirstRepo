dec4d153d432b2cfa9b2a80a691c29b1bdec7fe5
Rahangdale <Hitesh.Rahangdale@mastercard.com>
2017-12-12 12:50:46 +0530

    My First Commit

---
+++ MyFirstHeroku/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFirstHeroku.Startup))]
+namespace MyFirstHeroku
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
